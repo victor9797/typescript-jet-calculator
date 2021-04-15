68f44f51105073ee11963806316d1cbb9c99e4b7
Victor Rodriguez <victor.i.rodriguez@oracle.com>
2021-04-15 14:00:19 -0500

    Version working :D

--- test/specs/basic-calculator-knockout.spec.ts
+++ test/specs/basic-calculator-knockout.spec.ts
@@ -27,23 +27,23 @@ describe('basic-calculator CCA unit tests with Knockout', () => {
    * Setup the fixture with the markup and re-query the "calculator" variable so that
    * each test case gets a fresh new copy of the element.
    */
-  async function setup() {
+  async function setup(firstNumber = '1', secondNumber = '2', operation = 'sum') {
     fixture.set(markup);
     calculator = document.querySelector('basic-calculator') as HTMLElement;
     expect(calculator).not.to.be.null;
     // Apply our viewmodel to the Knockout bindings
     ko.applyBindings({
-      firstnumber: '1',
-      secondnumber: '2',
-      operation: 'sum'
+      firstnumber: firstNumber,
+      secondnumber: secondNumber,
+      operation: operation
     }, calculator);
     // Wait for BusyContext to clear for the element
     await Context.getContext(calculator).getBusyContext().whenReady();
   }
 
-  describe('UI tests', () => {
+  describe('Sum tests', () => {
     // Setup the fixture before each test case
-    beforeEach(async () => await setup());
+    beforeEach(async () => await setup('1', '2', 'sum'));
 
     it('Calculator gets loaded', async () => {
       await Context.getContext(calculator).getBusyContext().whenReady();
@@ -67,11 +67,101 @@ describe('basic-calculator CCA unit tests with Knockout', () => {
       expect(resultValue.innerText).to.be.equal('3');
     });
 
-    // function sleep(ms) {
-    //   return new Promise(resolve => setTimeout(resolve, ms));
-    // }
+    afterEach(() => fixture.cleanup());
+  });
+
+
+  describe('Substract tests', () => {
+    // Setup the fixture before each test case
+    beforeEach(async () => await setup('5', '2', 'substract'));
+
+    it('Calculator gets loaded', async () => {
+      await Context.getContext(calculator).getBusyContext().whenReady();
+      expect(calculator).not.to.be.null;
+    });
+
+    it('Verify substract operation', async () => {
+      // Arrange
+      const constButton = calculator.querySelector('#icon_button') as HTMLElement;
+      expect(constButton).not.to.be.null;
+
+      // Act
+      constButton.click();
+      await Context.getContext(calculator).getBusyContext().whenReady();
+
+      // Assert
+      const result = calculator.querySelector('#result') as HTMLElement;
+      expect(result).not.to.be.null;
+      const resultValue = result.querySelector('.oj-text-field-readonly') as HTMLElement;
+      expect(resultValue).not.to.be.null;
+      expect(resultValue.innerText).to.be.equal('3');
+    });
+
+    afterEach(() => fixture.cleanup());
+  });
+
+
+  describe('Multiply tests', () => {
+    // Setup the fixture before each test case
+    beforeEach(async () => await setup('5', '2', 'multiply'));
+
+    it('Calculator gets loaded', async () => {
+      await Context.getContext(calculator).getBusyContext().whenReady();
+      expect(calculator).not.to.be.null;
+    });
+
+    it('Verify multiply operation', async () => {
+      // Arrange
+      const constButton = calculator.querySelector('#icon_button') as HTMLElement;
+      expect(constButton).not.to.be.null;
+
+      // Act
+      constButton.click();
+      await Context.getContext(calculator).getBusyContext().whenReady();
+
+      // Assert
+      const result = calculator.querySelector('#result') as HTMLElement;
+      expect(result).not.to.be.null;
+      const resultValue = result.querySelector('.oj-text-field-readonly') as HTMLElement;
+      expect(resultValue).not.to.be.null;
+      expect(resultValue.innerText).to.be.equal('10');
+    });
+
+    afterEach(() => fixture.cleanup());
+  });
+
+
+  describe('Divide tests', () => {
+    // Setup the fixture before each test case
+    beforeEach(async () => await setup('100', '2', 'divide'));
+
+    it('Calculator gets loaded', async () => {
+      await Context.getContext(calculator).getBusyContext().whenReady();
+      expect(calculator).not.to.be.null;
+    });
+
+    it('Verify divide operation', async () => {
+      // Arrange
+      const constButton = calculator.querySelector('#icon_button') as HTMLElement;
+      expect(constButton).not.to.be.null;
+
+      // Act
+      constButton.click();
+      await Context.getContext(calculator).getBusyContext().whenReady();
+
+      // Assert
+      const result = calculator.querySelector('#result') as HTMLElement;
+      expect(result).not.to.be.null;
+      const resultValue = result.querySelector('.oj-text-field-readonly') as HTMLElement;
+      expect(resultValue).not.to.be.null;
+      expect(resultValue.innerText).to.be.equal('50');
+    });
 
     afterEach(() => fixture.cleanup());
   });
 
 });
+
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
